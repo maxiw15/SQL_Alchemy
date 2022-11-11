1b58ce960e979adf54d6bbb107dd59b6b5d6f594
maks <max.iw15@yandex.ru>
2022-11-11 21:23:53 +0300

    Задание 3 finish

--- main.py
+++ main.py
@@ -1,27 +1,18 @@
 import sqlalchemy
-import json
 from sqlalchemy.orm import sessionmaker
 from CONFIG import DSN
-from tables import Base, Publisher, Shop, Book, Stock, Sale, create_tables
+from tables import create_tables, insert
 
-engine = sqlalchemy.create_engine(DSN)
-create_tables(engine)
 
-Session = sessionmaker(bind=engine)
-session = Session()
+def select(publisher):
+    None
 
-with open('data.json', 'r') as fd:
-    data = json.load(fd)
+if __name__ == '__main__':
+    engine = sqlalchemy.create_engine(DSN)
+    create_tables(engine) # создаем таблицу
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    insert(session) # Добавляем значения в таблицу
 
-for record in data:
-    print(record)
-    model = {
-        'publisher': Publisher,
-        'shop': Shop,
-        'book': Book,
-        'stock': Stock,
-        'sale': Sale,
-    }[record.get('model')]
 
-    session.add(model(id=record.get('pk'), **record.get('fields')))
-session.commit()
+
--- tables.py
+++ tables.py
@@ -1,5 +1,6 @@
 import sqlalchemy as sq
 from sqlalchemy.orm import declarative_base, relationship
+import json
 
 Base = declarative_base()
 
@@ -55,3 +56,20 @@ class Sale(Base):
 def create_tables(engine):
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
+
+
+def insert(session):
+    with open('data.json', 'r') as fd:
+        data = json.load(fd)
+
+    for record in data:
+        model = {
+            'publisher': Publisher,
+            'shop': Shop,
+            'book': Book,
+            'stock': Stock,
+            'sale': Sale,
+        }[record.get('model')]
+
+        session.add(model(id=record.get('pk'), **record.get('fields')))
+    session.commit()
